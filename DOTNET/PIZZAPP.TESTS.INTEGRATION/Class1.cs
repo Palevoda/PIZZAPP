fc8eccf839898980b4dc44eaa015227f0e48fde1
Palevoda <alex-pale@mail.ru>
2021-09-27 13:59:46 +0300

    Add project files.

---
+++ DOTNET/PIZZAPP.TESTS.INTEGRATION/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PIZZAPP.TESTS.INTEGRATION
+{
+    public class Class1
+    {
+    }
+}
---
+++ DOTNET/PIZZAPP.TESTS.UNIT/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PIZZAPP.TESTS.UNIT
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
